87401ca802dbb0692948d2c7849b7890fd813547
Witek Bedyk <witold.bedyk@suse.com>
2021-03-16 12:59:09 +0100

    Remove vendor dir and update configuration
    Signed-off-by: Witek Bedyk <witold.bedyk@suse.com>

--- config/config_test.go
+++ config/config_test.go
@@ -757,8 +757,6 @@ var expectedConf = &Config{
 					Host:              "http://example.uyuni-project.org",
 					User:              "gopher",
 					Pass:              "hole",
-					ExporterFormulas:  []string{"prometheus-exporters"},
-					FormulasSeparator: ",",
 					RefreshInterval:   model.Duration(60 * time.Second),
 				},
 			},
